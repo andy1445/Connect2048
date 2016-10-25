9a6797c7b1a34161f971f38e96343fa528ca7839
andy1445 <gaoandy1445@gmail.com>
2016-10-25 14:04:52 -0400

    removed temp (useless files)

--- temp/Game.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by andy1445 on 10/5/2016.
- */
-public class Game {
-    public static void main(String[] args) {
-        Connect4 game1 = new Connect4();
-        game1.play();
-    }
-}
--- temp/MatrixTest.java
+++
@@ -1,8 +0,0 @@
-import static org.junit.Assert.*;
-
-/**
- * Created by andy1445 on 10/3/2016.
- */
-public class MatrixTest {
-
-}
